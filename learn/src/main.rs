475da556968555d1fa78a6220d0f9fda540d8864
ImSreyas <sreyassatheesh755@gmail.com>
2024-05-06 17:50:07 +0530

    new code added.

--- learn/src/main.rs
+++ learn/src/main.rs
@@ -4,6 +4,43 @@ use std::io::{Write};
 use std::cmp::Ordering;
 
 fn main(){
+    // guess();
+    // looping();
+    pos();
+}
+
+fn pos() {
+    loop {
+        print!("Enter a number : ");
+        io::stdout().flush().unwrap();
+        let mut val: String = String::new();
+        io::stdin().read_line(&mut val).unwrap();
+        let val: i32 = match val.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number.");
+                continue;
+            }
+        };
+        if val < 0 {
+            println!("The value is negative.");
+            continue;
+        } 
+        println!("value is : {}", val);
+    }
+}
+
+fn looping() {
+    let a = [10, 20, 30, 40];
+    let mut sum = 0;
+    for (index, value) in a.iter().enumerate() {
+        println!("value at {index} is : {value}");
+        sum += value;
+    }
+    println!("sum of the elements in the array is {:?} : {}", a, sum);
+}
+
+fn guess() {
     let random:i32 = rand::thread_rng().gen_range(1..101);
     println!("Random values is : {}", random);
     loop {
