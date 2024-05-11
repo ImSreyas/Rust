4b4ae06bf413b904690bbc9c7bd5ec5b3fec0ae1
ImSreyas <sreyassatheesh755@gmail.com>
2024-05-11 21:09:34 +0530

    Learning

---
+++ learn/src/generic_test.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+struct Rectangle<T, U> {
+    width: T,
+    height: U
+}
+
+impl<T, U> Rectangle<T, U> {
+    fn mix<V, W>(&self, other: &Rectangle<V, W>) -> Rectangle<&T, &W> {
+        Rectangle {
+            width: &self.width, 
+            height: &other.height
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 40, 
+        height: 30
+    };   
+    let rect2 = Rectangle {
+        width: 10.0,
+        height: 20.0
+    };
+    let mut rect3 = rect1.mix(&rect2);
+    println!("{:?}", rect3);
+    rect3.height = &15.0;
+    println!("{:?}", rect3);
+    println!("{:?}", rect2);
+}
