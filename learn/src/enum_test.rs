35e329c82b6915ca4266effb43f277b5361b5ff4
ImSreyas <sreyassatheesh755@gmail.com>
2024-06-21 00:36:24 +0530

    Learning

--- learn/src/enum_test.rs
+++ learn/src/enum_test.rs
@@ -1,13 +1,8 @@
 #[derive(Debug)]
 enum Ip {
-    v4{
-        x: u8,
-        y: u8
-    },
-    v6(String)
+    V6(String)
 }
 fn main() {
-    let a: String = "Hello".to_string();
-    let val = Ip::v4 {x: 12, y: 20};
+    let val = Ip::V6(String::from("hello"));
     println!("value is : {:?}", val);
 }
